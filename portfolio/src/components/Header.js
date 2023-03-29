9e1aecc7ee829b5a7102683b356bba051a8b9c59
Dschnepf7 <domschnepf1@gmail.com>
2023-03-28 21:18:27 -0500

    nested navbar within header

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import Navbar from './Navbar';
 function Header(){
     return (
         <div>
-            <h1>Dominic</h1>
+            <Navbar/>
         </div>
     )
     
