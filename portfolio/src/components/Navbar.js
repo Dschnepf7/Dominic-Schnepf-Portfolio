d81543959ab2b4701d8c2bb2bfe7e7f1996ff86d
Dschnepf7 <domschnepf1@gmail.com>
2023-03-29 18:21:36 -0500

    added functionality to navbar

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -1,35 +1,59 @@
-import React from 'react';
+import React, { useState } from "react";
+import AboutMe from "./AboutMe";
+import Portfolio from "./Portfolio";
+import Contact from "./Contact";
+import Resume from "./Resume";
 
 function Navbar() {
+  const [selected, setSelected] = useState(null);
+
+  const items = [
+    { label: "About Me", component: <AboutMe /> },
+    { label: "Portfolio", component: <Portfolio /> },
+    { label: "Contact", component: <Contact /> },
+    { label: "Resume", component: <Resume /> }
+  ];
+
+  const handleClick = (index) => {
+    setSelected(index);
+  };
+
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Dominic Schnepf</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Dominic Schnepf
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="https://github.com/Dschnepf7">Github</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
-              </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><hr class="dropdown-divider" /></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-              </ul>
-            </li>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            {items.map((item, index) => (
+              <li className="nav-item" key={index}>
+                <a
+                  className={`nav-link ${
+                    selected === index ? "active" : ""
+                  }`}
+                  onClick={() => handleClick(index)}
+                  href="#"
+                >
+                  {item.label}
+                </a>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
+      {selected !== null && items[selected].component}
     </nav>
   );
 }
