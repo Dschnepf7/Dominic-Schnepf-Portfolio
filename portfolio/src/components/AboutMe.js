4216500f47608a6020e4bab89e7f25486ecd7490
Dschnepf7 <domschnepf1@gmail.com>
2023-03-30 21:41:23 -0500

    added all pictures and links

--- portfolio/src/components/AboutMe.js
+++ portfolio/src/components/AboutMe.js
@@ -1,17 +1,21 @@
 import React from 'react';
 
-function AboutMe(){
+function AboutMe() {
     return (
         <div>
             <div class="container-lg">
-            <div>
-                <h1 class="about-me">About Me</h1>
-                <h2 class="introduction">Hi there! Thanks for visiting my website. My name is Dominic Schnepf. I am a coding bootcamp student, currently in the process of learning React.js. I started my web development journey about 3 months ago and am amazed at how much I've learned! Feel free to check out my portfolio section to see the projects I've worked on throughout my short coding career. Thanks again for stopping by! </h2>
-            </div>
+
+                <div>
+                    <h1 class="about-me">About Me</h1>
+                    <div class="text-center">
+                        <img src="./project-images/Hero-photo.png" class="rounded" alt="headshot"/>
+                    </div>
+                    <h2 class="introduction">I am from Ankeny, IA, just outside of Des Moines. I went to school at Iowa Western Community College and transferred to the University of Northern Iowa to finish my undergraduate in psychology. Through all four years of college I had the opportunity to play for both of my schools' golf teams. I graduated from UNI in the winter of 2019 and began a career in the golf industry. The game of golf will always be special to me, but I loved it more as a sport and less as a job. I have always been around technology and have had a decent aptitude for it. I always found what developers do to be very interesting and wondered if I could do it myself. I took a step towards answering that question by enrolling in Northwestern University's coding bootcamp and have been amazed at the skills I have learned. I am eager to see where this journey goes in the future! </h2>
+                </div>
             </div>
         </div>
     )
-    
+
 }
 
 export default AboutMe;
--- portfolio/src/components/Project.js
+++ portfolio/src/components/Project.js
@@ -4,54 +4,67 @@ function Project() {
     return (
 
         <div class="container text-center">
-            <div class="row">
+            <h2 class="project-heading">Click the images to take a look at the Github Repository for each project.</h2>
+            <div class="row top-row">
                 <div class="col">
-                    <div class="card" style={{ width: '18rem' }}>
-                        <img src="https://placebeard.it/640x360" class="card-img-top" alt="placeholder image" />
+                    <div class="card image-card" style={{ width: '20rem' }}>
+                        <a href= "https://github.com/Dschnepf7/Social-Media-API">
+                        <img src="../project-images/Screenshot-for-assignment18.png" class="card-img-top" alt="placeholder image" />
+                        </a>
                         <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <p class="card-text">An example of the social media rest API using the insomnia app to display each CRUD method.</p>
                         </div>
                     </div>
                 </div>
                 <div class="col">
-                    <div class="card" style={{ width: '18rem' }}>
-                        <img src="https://placebeard.it/640x360" class="card-img-top" alt="placeholder image" />
+                    <div class="card image-card" style={{ width: '20rem' }}>
+                    <a href="https://github.com/Dschnepf7/JATE-Text-Editor">
+                        <img src="./project-images/Jate-Screenshot.png" class="card-img-top" alt="placeholder image" />
+                        </a>
                         <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <p class="card-text">JATE (Just Another Text Editor) is just what it sounds like! Write, save and retrieve notes with this application.</p>
                         </div>
                     </div>
                 </div>
                 <div class="col">
-                    <div class="card" style={{ width: '18rem' }}>
-                        <img src="https://placebeard.it/640x360" class="card-img-top" alt="placeholder image" />
+                    <div class="card image-card" style={{ width: '20rem' }}>
+                    <a href="https://github.com/Dschnepf7/Work-Day-Scheduler">
+                        <img src="./project-images/Workday-Scheduler.png" class="card-img-top" alt="placeholder image" />
+                        </a>
                         <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <p class="card-text">A Workday scheduler application that lets you add and delete tasks, as well as keeps trach of the time of day.</p>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="row">
-                <div class="col">
-                    <div class="card" style={{ width: '18rem' }}>
-                        <img src="https://placebeard.it/640x360" class="card-img-top" alt="placeholder image" />
+                <div class="col image-column">
+                    <div class="card image-card" style={{ width: '20rem' }}>
+                        <a href="https://github.com/MattOz/Pupify">
+                        <img src="./project-images/demo3.png" class="card-img-top" alt="placeholder image" />
+                        </a>
                         <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <p class="card-text">An example of utilizing third party API's. Every dog lover should check out this appliation!</p>
                         </div>
                     </div>
                 </div>
                 <div class="col">
-                    <div class="card" style={{ width: '18rem' }}>
-                        <img src="https://placebeard.it/640x360" class="card-img-top" alt="placeholder image" />
+                    <div class="card image-card" style={{ width: '20rem' }}>
+                        <a href="https://github.com/Dschnepf7/README-Generator">
+                        <img src="../project-images/ReadMe-Generator.png" class="card-img-top" alt="placeholder image" />
+                        </a>
                         <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <p class="card-text">A backend application to generate professional ReadMe.md files. Utilizes the Inquirer npm package to take user input.</p>
                         </div>
                     </div>
                 </div>
                 <div class="col">
-                    <div class="card" style={{ width: '18rem' }}>
-                        <img src="https://placebeard.it/640x360" class="card-img-top" alt="placeholder image" />
+                    <div class="card image-card" style={{ width: '20rem' }}>
+                        <a href="https://github.com/Dschnepf7/NoteTaker">
+                        <img src="./project-images/Note-Taker.png" class="card-img-top" alt="placeholder image" />
+                        </a>
                         <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <p class="card-text">A note taking app that saves notes written by the user to a database for retrieval at a later date.</p>
                         </div>
                     </div>
                 </div>
